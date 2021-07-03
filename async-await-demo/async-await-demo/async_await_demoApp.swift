f538b02cafba8fbb41a6ff8c09fcca72bb71f716
Alexey <alexey.voronov96@gmail.com>
2021-07-04 00:12:58 +0500

    project setup

---
+++ async-await-demo/async-await-demo/async_await_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_await_demoApp.swift
+//  async-await-demo
+//
+//  Created by Алексей Воронов on 04.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct async_await_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
